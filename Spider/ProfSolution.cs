0e17628d2f4efaac8baf7ff47182964656b562af
bobaldo <davidepatrizi@gmail.com>
2014-12-12 14:45:08 +0100

    add spider

---
+++ Spider/ProfSolution.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spider
+{
+    class ProfSolution
+    {
+        static WebClient wc = new WebClient();
+        static Queue<Tuple<string, int>> spiderq = new Queue<Tuple<string, int>>();
+
+        static IEnumerable<string> ParseDoc(string url, int i)
+        {
+            var data = wc.DownloadString(url);
+            var match = 
+            foreach (var item in collection)
+            {
+                
+            }
+        }   
+
+        static void Main(string[] args)
+        {
+            var maxDepth =2;
+            spiderq.Enqueue(new Tuple<string, int>("http://repubblica.it/,",0));
+
+            using (var ctxt = new SpiderEntities())
+            {
+                while (spiderq.Count > 0)
+                {
+                    var u = spiderq.Dequeue();
+                    wc.DownloadString(u.Item1);
+                    var data = new Spider.url();
+                    data.depth = 1;
+                    data.content = c;
+                    data.type = "text/html";
+                    ctxt.urls.Add(data);
+                    ctxt.SaveChanges();
+                    if (data.depth < maxDepth)
+                        foreach (var u in ParseDoc(c))
+                            spiderq.Enqueue(new Tuple<string, int>(u, data.depth + 1));
+                }
+            }
+        }
+    }
+}
---
+++ Spider/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Spider
+{
+    /*scrivere uno spider web che prende un'insieme di url web, analizza le pagina cercando le url 
+    e le vado a vedere */
+    class Program
+    {
+        private const string EXPRESSION = @"http://[w+\./\?=\+\-\%]+";
+        private static HttpClient wp = new HttpClient();
+
+        static async Task<string[]> GetUrlsAsync(string url)
+        {
+            
+            var tb = new StringBuilder();
+            var tasks = wp.GetStringAsync(url);
+            tb.Append(await tasks);
+            var mat  = Regex.Matches(tb.ToString(), EXPRESSION);
+
+            foreach (var m in mat)
+            {
+                write
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var maxDepth = 5;
+            var sites = new string[] { "http://www.davidepatrizi.com" };
+
+
+
+            foreach (var i in sites)
+            {
+                var urls = GetUrlsAsync(i);
+
+                foreach (var a in urls.Result)
+                {
+                    write(1, a, "aaa", "bbb");
+                }
+            }
+        }
+
+        private static void write(int depth, string uri, string type, string content)
+        {
+            using (var ctxt = new SpiderEntities())
+            {
+                var u = new Spider.url();
+                u.depth = depth;
+                u.type = type;
+                u.uri = uri;
+                u.content = content;
+                ctxt.urls.Add(u);
+                ctxt.SaveChanges();
+
+                //query
+                //var q = from uri in ctxt.urls where uri.depth == 0 select uri;
+            }
+        }
+    }
+}
