99249b20369b555b76712277ccbf824396d0a6e1
bobaldo <davidepatrizi@gmail.com>
2015-02-27 13:41:58 +0100

    add lezione 27022015

---
+++ CollectionsProject/CollectionsProject/CollectionsProject.Shared/LocalFolderNav.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace CollectionsProject
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class LocalFolderNav : Page
+    {
+        public static DependencyProperty CurrentFolderFQNProperty = DependencyProperty.Register("CurrentFolderFQN", typeof(String), typeof(LocalFolderNav), null);
+
+        public string CurrentFolderFQN
+        {
+            get { return (string)GetValue(CurrentFolderFQNProperty); }
+            set { SetValue(CurrentFolderFQNProperty, value); }
+        }
+
+        ObservableCollection<StorageFolder> folders;
+        StorageFolder currentFolder;
+
+        public LocalFolderNav()
+        {
+            this.InitializeComponent();
+            currentFolder = ApplicationData.Current.LocalFolder;
+            CurrentFolderFQN = currentFolder.Path + "/" + currentFolder.Name;
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            folders = new ObservableCollection<StorageFolder>(await currentFolder.GetFoldersAsync());
+            CurrentFolderBox.ItemsSource = folders;
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (await ApplicationData.Current.LocalFolder.GetFolderAsync("Prova3") == null)
+                {
+                    var s = await ApplicationData.Current.LocalFolder.CreateFolderAsync("Prova3");
+                    folders.Add(s);
+                }
+            }
+            catch { }
+        }
+
+        private async void TextBlock_DoubleTapped(object sender, DoubleTappedRoutedEventArgs e)
+        {
+            var tb = sender as TextBlock;
+            var f = tb.DataContext as StorageFolder;
+            folders = new ObservableCollection<StorageFolder>(await f.GetFoldersAsync());
+            currentFolder = f;
+            CurrentFolderFQN = currentFolder.Path + "/" + f.Name;
+            CurrentFolderBox.ItemsSource = folders;
+        }
+    }
+}
---
+++ CollectionsProject/CollectionsProject/CollectionsProject.Windows/MainPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace CollectionsProject
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {        
+        ObservableCollection<Person> obData;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Person[] data = new Person[] {
+                new Person{Nome="Antonio", Cognome="Cisternino"},
+                new Person{Nome="Davide", Cognome="Patrizi"}
+            };
+
+            obData = new ObservableCollection<Person>(data);
+            Persone.ItemsSource = obData;
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var f = Windows.Storage.ApplicationData.Current.LocalFolder;
+            Persone.ItemsSource = await f.GetFilesAsync();
+
+            obData.Add(new Person { Nome = "AAA", Cognome = "AAAA" });
+            obData[1].Cognome = "Gervasi";
+        }
+    }
+
+    public class Person : INotifyPropertyChanged
+    {
+        private string nome;
+
+        public string Nome
+        {
+            get { return nome; }
+            set
+            {
+                nome = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string cognome;
+
+        public string Cognome
+        {
+            get { return cognome; }
+            set
+            {
+                cognome = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
